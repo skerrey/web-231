d166d1724b3b2c236de84db46bd71d792ffea077
skerrey <sethkerrey@gmail.com>
2021-12-03 10:49:15 -0600

    Add JS initial comments

---
+++ week-8/kerrey-alphabet-game.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title: Assignment 8.2 Alphabet Game
+; File Name: kerrey-alphabet-game.js
+; Author: Professor Krasso
+; Date: 12 December 2021
+; Modified By: Seth Kerrey
+; Description: 
+;   Script for the kerrey-alphabet-game.html file
+; Resources: 
+;   JavaScript code was taken from "WEB 231 Assign_8.docx" 
+;   	Class names, constructor parameters
+;		& variable names provided by Prof Krasso.
+;
+===========================================
+*/
