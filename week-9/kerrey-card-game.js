991fc8539f5b1032081cb702ef03799a4cb81593
skerrey <sethkerrey@gmail.com>
2021-12-15 17:28:05 -0600

    initial comments

---
+++ week-9/kerrey-card-game.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title: Assignment 9.2 Final: Card Game
+; File Name: kerrey-card-game.js
+; Author: Professor Krasso
+; Date: 19 December 2021
+; Modified By: Seth Kerrey
+; Description:
+;   Script for the kerrey-card-game.html file.
+; Resources:
+;   JavaScript code was taken from "WEB 231 Assign_8.docx"
+;   	Class names, ids, & variable names provided by Prof Krasso.
+;
+===========================================
+*/
