04b1efb8dac6d3d2119deb27670aeb134571c389
skerrey <sethkerrey@gmail.com>
2021-12-15 19:48:08 -0600

    Update kerrey-card-game.js

--- week-9/kerrey-card-game.js
+++ week-9/kerrey-card-game.js
@@ -10,6 +10,91 @@
 ; Resources:
 ;   JavaScript code was taken from "WEB 231 Assign_8.docx"
 ;   	Class names, ids, & variable names provided by Prof Krasso.
-;
+;   JS code provided by Prof Krasso:
+;       this.cards[count] = new Card(this.faces[count % 13], this.suits[Math.floor(count / 13)]);
+;       Math.floor(Math.random() * this.CARD_COUNT);
+;   W3Schools JavaScript Function
+;       https://www.w3schools.com/js/js_functions.asp
+;   W3Schools JS Function Call
+;       https://www.w3schools.com/js/js_function_call.asp
 ===========================================
 */
+
+// Card class
+class Card {
+    constructor(face, suit) {
+      this.face = face;
+      this.suit = suit;
+    } 
+}
+
+// Dealer class
+class Dealer {
+    constructor() {
+        getDeckOfCards();
+    } 
+}
+
+// Assign variables
+let CARD_COUNT = 52;
+
+// Assign array cards, faces, suits
+const cards = [];
+const faces = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+
+// Function getDeckOfCards, code provided by Prof Krasso
+function getDeckOfCards() {
+    for (let count=0; count<this.CARD_COUNT; count++) {
+        this.cards[count] = new Card(this.faces[count % 13], this.suits[Math.floor(count / 13)]);
+    }
+}
+
+
+function shuffle() {
+    for (let count=0; count<this.CARD_COUNT; count++) {
+        let tempCard = this.cards[firstCard];
+        
+        let secondCard = Math.floor(Math.random() * this.CARD_COUNT);
+        cards[firstCard] = cards[secondCard];
+        cards[secondCard] = tempCard;
+    }
+    return shuffle;
+}
+
+function buildPlayerCard(card, suitIcon, faceColor, suitColor) {
+    `<div class=”card player-card”><div class=”card-title” style=”color: ${faceColor}”>${card.face}</div></div> `;
+    
+}
+
+// Register onclick event
+document.getElementById("btnDealCards").onclick = function() {
+
+    let Dealer = {cards};
+    shuffle();
+    function named() {}
+    let cardOutputWithIcon = "";
+    for(let x of Dealer.cards) {
+        switch(card.suit) {
+
+            case "Hearts":
+                cardOutputWithIcon += buildPlayingCard(card, "mdi", "mdi-heart", "red", "red");
+                console.log("hearts");
+                break;
+            case "Diamonds":
+                cardOutputWithIcon += buildPlayingCard(card, "mdi mdi-cards-diamonds", "red", "red");
+                break;
+            case "Clubs":
+                cardOutputWithIcon += buildPlayingCard(card, "mdi mdi-cards-club", "black", "black");
+                break;
+            case "Spades":
+                cardOutputWithIcon += buildPlayingCard(card, "mdi mdi-spade", "black", "black");
+                break;
+            default:
+                console.log("Default");
+        }
+
+    }
+
+}
+
